900dc3930d8ba46b8bd9d5f806fdeb06f9566f38
米田崇人 <takahito.yoneda@atrae.co.jp>
2020-01-15 08:29:29 +0900

    test

---
+++ src/Congrats.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default (props) => {
+        if (props.success) {
+            return(
+                <div data-test="component-congrats">
+                    <span data-test="congrats-message">
+                        Congrats! You guessed the word!
+                    </span>
+                </div>
+            )
+        } else {
+            return (
+                <div data-test="component-congrats" />
+            )
+        }
+}
