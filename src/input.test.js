4de34e908d3b415ec658ff8ce738616b04434231
米田崇人 <takahito.yoneda@atrae.co.jp>
2020-01-16 09:22:06 +0900

    complete render tests

--- src/input.test.js
+++ src/input.test.js
@@ -12,14 +12,24 @@ const setup = (initialState={}) => {
 
 describe('render', () => {
  describe('word has not been guessed', () => {
-    test('renders component without error', () => {
-
+    let wrapper;
+    beforeEach(() => {
+       const initialState = { success: false };
+       wrapper = setup(initialState);
     });
-    test('does not renders input box', () => {
 
+    test('renders component without error', () => {
+        const component = findByTestAttr(wrapper, "component-input");
+        expect(component.length).toBe(1);
+    });
+
+    test('does not renders input box', () => {
+        const inputBox = findByTestAttr(wrapper, "input-box");
+        expect(inputBox.length).toBe(1);
     });
     test('does not render submit button', () => {
-
+        const submitButton = findByTestAttr(wrapper, "submit-button");
+        expect(submitButton.length).toBe(1);
     });
  });
 
